c841fadf3068da64fb6d6d929987c6a592a8fae4
M G Pathum Kaushallaya <mgpdesaman@gmail.com>
2026-08-24 14:43:38 +0530

    Integrated Nerdtech License Verification middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\VerifyLicense;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'check.status' => \App\Http\Middleware\CheckAccountStatus::class,
         ]);
         $middleware->append(\App\Http\Middleware\CheckAccountStatus::class);
+        $middleware->web(append: [
+            VerifyLicense::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
